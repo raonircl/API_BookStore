d7ddd169336a65c8cdc646a6cdb824e86e7efca7
raonircl <raoni.afi@gmailcom>
2024-04-05 06:14:40 -0300

    feat: adding authenticate method to model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
+
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
+
   validates :name, presence: true, format: { without: /\d/ }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 8 }
+
+  def authenticate(password)
+    BCrypt::Password.new(self.password) == password
+  end
 end
