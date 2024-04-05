1188ad25568555cb193d64c593626057a7d2e141
raonircl <raoni.afi@gmailcom>
2024-04-05 05:29:26 -0300

    feat: user.rb validations: username, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }
 end
